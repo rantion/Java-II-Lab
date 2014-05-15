894602b6289fecfd45307154760a69de48d45e82
rantion <rachacha>
2014-05-15 13:29:48 -0600

    changed to be appropriate

--- src/wellEndowed/Main.java
+++ src/wellEndowed/Main.java
@@ -3,7 +3,7 @@ package wellEndowed;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("TK is a bitch");
+		System.out.println("TK is a cool kid");
 		System.out.println("I agree");
 	}
 
