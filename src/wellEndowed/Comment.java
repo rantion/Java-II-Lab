5567d2ae6bc8bb34f05026af456f6d0c01c627b8
rantion <rachacha>
2014-05-14 22:37:03 -0600

    Comment fixed.

--- src/wellEndowed/Comment.java
+++ src/wellEndowed/Comment.java
@@ -1,5 +1,17 @@
 package wellEndowed;
 
+import org.joda.time.DateTime;
+
 public class Comment {
+	
+	private User poster;
+	private Post postingOn;
+	private Comment commentPostingOn;
+	private String content;
+	private DateTime postTime;
+	
+	public Comment(){
+		
+	}
 
 }
--- src/wellEndowed/Message.java
+++ src/wellEndowed/Message.java
@@ -1,6 +1,14 @@
 package wellEndowed;
 
+import java.util.List;
+
+import org.joda.time.DateTime;
+
 public class Message {
+	
+	private List<User> senders, recievers;
+	private DateTime dateSent;
+	private boolean isGroupMessage;
 	
 	public Message(){
 		
